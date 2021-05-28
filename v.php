1a9f289d23c3cef734065be49b1725622361fbee
zengxiangyou <1005592906@qq.com>
2021-05-28 14:53:30 +0800

    Add files via upload

---
+++ v.php
@@ -0,0 +1,13 @@
+<?php 
+
+//底层框架版本
+return array(
+    'v' => '1.06.2',
+    'time' => '2019-12-07',
+    'copyright' => '2015-2019',
+    'name' => 'zephp',
+    'link' => 'http://www.zephp.com/',
+    'link2' => 'http://www.zengxy.com/',
+    'author' => 'zengxy.com 1559261757@qq.com',
+    'copyrighted' => 'ZePHP网站开发系统（登记号：2018SR847284）'
+);
